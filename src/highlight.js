01a462fed4f4e5965374923e3d8e3e5da10a55a0
Torben Gutermuth Name <torben.gutermuth@uni-hamburg.de>
2023-02-10 01:43:59 -0800

    Implement SVG substructure highlighting
    - substructure is determined by atom indexes
    - atoms get circles, bonds are underlaid

---
+++ src/highlight.js
@@ -0,0 +1,225 @@
+/**
+ * Highlight substructure in SVG element. Substructures are all atoms with the
+ * give atom indexes and the bonds between them. Substructures of single atom
+ * indices result in either the highlighting of an explicit atom (the atom
+ * symbol in the SVG) or the highlighting of the meeting point of two bonds in
+ * a chain which marks its position.
+ * @param {SVGElement} svgElement - element to highlight substructure in
+ * @param {Array<number>} atomIndexes - atom indexes that make up the
+ *   substructure
+ */
+function highlightSubstructure (svgElement, atomIndexes) {
+  const backgroundRect = svgElement.getElementsByTagName('rect')[0]
+  const [atoms, bonds] = getSubstructureElements(svgElement, atomIndexes)
+  if (atoms.size === 0 && atomIndexes.length === 1) {
+    const containingElements = svgElement.querySelectorAll('.atom-' + atomIndexes[0])
+    const atomPosition = getAtomPositionFromBonds('atom-' + atomIndexes[0], containingElements)
+    const atomMarker = createAtomMarkerPosition(atomPosition, 12)
+    svgElement.insertBefore(atomMarker, backgroundRect.nextSibling)
+  }
+  for (const value of atoms.values()) {
+    value.forEach((atom) => {
+      const atomMarker = createAtomMarker(atom)
+      svgElement.insertBefore(atomMarker, backgroundRect.nextSibling)
+    })
+  }
+  for (const value of bonds.values()) {
+    value.forEach((bond) => {
+      const bondMarker = createBondMarker(bond)
+      svgElement.insertBefore(bondMarker, backgroundRect.nextSibling)
+    })
+  }
+}
+
+/**
+ * Get SVG elements of the substructure.
+ * @param {Array<number>} atomIndexes - atom indexes that make up the
+ *   substructure
+ * @returns {Array<Map<string, SVGElement>>} atoms and bonds maps
+ */
+function getSubstructureElements (svgElement, atomIndexes) {
+  atomIndexes = atomIndexes.sort()
+  const atoms = new Map()
+  const bonds = new Map()
+  for (let i = 0; i < atomIndexes.length; i++) {
+    for (let j = i + 1; j < atomIndexes.length; j++) {
+      const query = '.atom-' + atomIndexes[i] + '.atom-' + atomIndexes[j]
+      svgElement.querySelectorAll(query).forEach((element) => {
+        const classKey = element.classList.value
+        if (!bonds.has(classKey)) {
+          bonds.set(classKey, [])
+        }
+        bonds.get(classKey).push(element)
+      })
+    }
+    const atomClass = 'atom-' + atomIndexes[i]
+    const query = '.' + atomClass
+    svgElement.querySelectorAll(query).forEach((element) => {
+      const classKey = element.classList.value
+      if (classKey !== atomClass) {
+        return
+      }
+      if (!atoms.has(classKey)) {
+        atoms.set(classKey, [])
+      }
+      atoms.get(classKey).push(element)
+    })
+  }
+  return [atoms, bonds]
+}
+
+/**
+ * Get bounding box for a path element.
+ * @param {SVGElement} pathElement - path element to get a bounding box for
+ * @returns {Array<Array<number, 2>, 2>} axis-aligned bounding box of minimum 2D
+ *   coordinates to maximum coordinates
+ */
+function getPathBounds (pathElement) {
+  const path = pathElement.getAttribute('d')
+  let xMin, xMax, yMin, yMax
+  path.split('\n').forEach((pathInstruction) => {
+    const splitInstruction = pathInstruction.split(' ')
+    const xComponent = parseFloat(splitInstruction[1])
+    const yComponent = parseFloat(splitInstruction[2])
+    if (!xMin || xComponent < xMin) {
+      xMin = xComponent
+    }
+    if (!xMax || xComponent > xMax) {
+      xMax = xComponent
+    }
+    if (!yMin || yComponent < yMin) {
+      yMin = yComponent
+    }
+    if (!yMax || yComponent > yMax) {
+      yMax = yComponent
+    }
+  })
+  return [[xMin, yMin], [xMax, yMax]]
+}
+
+/**
+ * Create an SVG marker for an atom.
+ * @param {SVGElement} atom - atom SVG element
+ * @returns {SVGElement} SVG marker for an atom
+ */
+function createAtomMarker (atom) {
+  const boundingBox = getPathBounds(atom)
+  const [[xMin, yMin], [xMax, yMax]] = boundingBox
+  return createAtomMarkerPosition(
+    [xMin + (xMax - xMin) / 2, yMin + (yMax - yMin) / 2], // center
+    Math.max(xMax - xMin, yMax - yMin) // maximum in X or Y direction
+  )
+}
+
+/**
+ * Create an atom marker at a position.
+ * @param {Array<number, 2>} position - atom position to mark
+ * @param {number} radius - radius of the marker
+ * @returns {SVGElement} SVG marker for an atom
+ */
+function createAtomMarkerPosition (position, radius) {
+  const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+  circle.setAttributeNS(null, 'cx', position[0])
+  circle.setAttributeNS(null, 'cy', position[1])
+  circle.setAttributeNS(null, 'r', radius)
+  circle.setAttributeNS(null, 'fill', '#8080ff')
+  circle.setAttributeNS(null, 'class', 'substructure-highlight')
+  return circle
+}
+
+/**
+ * Create an SVG marker for a bond
+ * @param {SVGElement} bond - bond SVG element
+ * @returns {SVGElement} SVG marker for a bond
+ */
+function createBondMarker (bond) {
+  const clonedBond = bond.cloneNode(false)
+  clonedBond.style.stroke = '#8080ff'
+  clonedBond.style['stroke-width'] = '12px'
+  clonedBond.setAttributeNS(null, 'class', 'substructure-highlight')
+  return clonedBond
+}
+
+/**
+ * Get atom postion from bonds containing it. Useful for finding positions of
+ * chain atoms.
+ * @param {string} atomName - name of the atom
+ * @param {Array<SVGElement>} bondPaths - bond SVG elements containing the atom
+ * @returns {Array<number, 2>} position of the atom contained in the bonds
+ */
+function getAtomPositionFromBonds (atomName, bondPaths) {
+  const combinedBonds = combineBonds(bondPaths)
+  let possibleAtomPoints = []
+  for (const value of combinedBonds) {
+    const currentAtomPositions = []
+    for (const bond of value) {
+      const [fromPosition, toPosition] = getBondAtomPositions(bond)
+      if (bond.classList[1] === atomName) {
+        currentAtomPositions.push(fromPosition)
+      } else {
+        currentAtomPositions.push(toPosition)
+      }
+    }
+
+    if (value.length === 1) {
+      return currentAtomPositions[0]
+    }
+    if (possibleAtomPoints.length === 0) {
+      possibleAtomPoints = currentAtomPositions.slice()
+    } else {
+      return getCommonPoint(currentAtomPositions, possibleAtomPoints)
+    }
+  }
+}
+
+/**
+ * Combine bond elements to collection representing the same bond. Bonds to
+ * heteroatoms can be split into two to color one half differently. Bonds
+ * consisting of one element are sorted to the front.
+ * @param {Array<SVGElement>} bondPaths - bond SVG elements to combine to bonds
+ * @returns {Array<Array<SVGElement>>} combined bond elements
+ */
+function combineBonds (bondPaths) {
+  const combinedBonds = new Map()
+  for (const bond of bondPaths) {
+    bond.classList.forEach((cssClass) => {
+      if (cssClass.includes('atom')) {
+        return
+      }
+      if (!combinedBonds.has(cssClass)) {
+        combinedBonds.set(cssClass, [])
+      }
+      combinedBonds.get(cssClass).push(bond)
+    })
+  }
+  return Array.from(combinedBonds.values()).sort((a, b) => a.length - b.length)
+}
+
+/**
+ * Get positions of the from and too atoms of a bond.
+ * @param {SVGElement} bondPath - bond SVG element
+ * @returns {Array<Array<number, 2>, 2>} from and too positions
+ */
+function getBondAtomPositions (bondPath) {
+  const splitInstructions = bondPath.getAttribute('d').split(' ')
+  const fromPosition = splitInstructions[1].split(',').map(parseFloat)
+  const toPosition = splitInstructions[3].split(',').map(parseFloat)
+  return [fromPosition, toPosition]
+}
+
+/**
+ * Get the first common point.
+ * @param {Array<Array<number, 2>>} points - array of points
+ * @param {Array<Array<number, 2>>} otherPoints - other array of points
+ * @returns {Array<number, 2>} first common point
+ */
+function getCommonPoint (points, otherPoints) {
+  for (const point of points) {
+    for (const otherPoint of otherPoints) {
+      // array equal
+      if (point.every((val, index) => val === otherPoint[index])) {
+        return point
+      }
+    }
+  }
+}
